a7fddf3abe908e33ec137dad3e8d0773c047f39f
e-labinnovations <mohammedashad23@gmail.com>
2021-07-30 09:47:37 +0530

    dark mode detect added

--- js/app.js
+++ js/app.js
@@ -13,6 +13,23 @@ Router.addEventListener('ionRouteDidChange', (e) => {
     //presentToast(routerFrom + ' -> ' + routeTo);
 })
 
+if (localStorage.getItem('enableDarkMode') == null) {
+  // Check to see if Media-Queries are supported
+  if (window.matchMedia) {
+    // Check if the dark-mode Media-Query matches
+    if (window.matchMedia('(prefers-color-scheme: dark)').matches) {
+      // Dark
+      localStorage.setItem('enableDarkMode', true);
+    } else {
+      // Light
+      localStorage.setItem('enableDarkMode', false);
+    }
+  } else {
+    // Default (when Media-Queries are not supported)
+    localStorage.setItem('enableDarkMode', false);
+  }
+}
+
 let enableDarkMode = localStorage.getItem('enableDarkMode')==="true"?true:false;
 if (enableDarkMode) {
   document.querySelector('body').classList.add('dark')
